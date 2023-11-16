927e50e05d4e67faba994b9ca3b091ff76553c78
XuZhong <1574030430@qq.com>
2023-11-16 18:29:10 +0800

    convert3

--- app/src/main/java/com/example/superjjj/EnglishActivity.java
+++ app/src/main/java/com/example/superjjj/EnglishActivity.java
@@ -3,9 +3,11 @@ package com.example.superjjj;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.transition.Slide;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class EnglishActivity extends AppCompatActivity {
@@ -14,6 +16,21 @@ public class EnglishActivity extends AppCompatActivity {
     private View converButtons;
     private View back;
     private TextView text;
+
+    private TextView num;
+    private TextView english;
+    private String expression;
+
+    private Button[] buttons = new Button[12];
+    private Button[] function = new Button[3];
+
+    Button button0, button00, button1;
+    Button button2, button3, button4, button5, button6, button7;
+    Button button8, button9;
+    Button buttonDot;
+    Button clearAll, clear, equal;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,16 +50,114 @@ public class EnglishActivity extends AppCompatActivity {
         text = findViewById(R.id.textView);
         text.setText("ENGLISH");
 
+        num = findViewById(R.id.NumText);
+        english = findViewById(R.id.EngText);
+
+        num.setMovementMethod(new ScrollingMovementMethod());
+        english.setMovementMethod(new ScrollingMovementMethod());
+
         back = findViewById(R.id.backButton);
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 finish();
                 overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
             }
         });
+
+        button0 = findViewById(R.id.button0);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
+        button5 = findViewById(R.id.button5);
+        button6 = findViewById(R.id.button6);
+        button7 = findViewById(R.id.button7);
+        button8 = findViewById(R.id.button8);
+        button9 = findViewById(R.id.button9);
+        button00 = findViewById(R.id.button00);
+        buttonDot = findViewById(R.id.buttonDot);
+        clearAll = findViewById(R.id.buttonClearAll);
+        clear = findViewById(R.id.buttonClear);
+        equal = findViewById(R.id.buttonEqual);
+
+        buttons[0] = button0;
+        buttons[1] = button1;
+        buttons[2] = button2;
+        buttons[3] = button3;
+        buttons[4] = button4;
+        buttons[5] = button5;
+        buttons[6] = button6;
+        buttons[7] = button7;
+        buttons[8] = button8;
+        buttons[9] = button9;
+        buttons[10] = button00;
+        buttons[11] = buttonDot;
+
+        function[0] = clearAll;
+        function[1] = clear;
+        function[2] = equal;
+
+        setupButtonListeners();
+    }
+
+    private void setupButtonListeners() {
+        View.OnClickListener buttonClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (num != null) {
+                    Button clickedButton = (Button) v;
+                    handleButtonClick(clickedButton);
+                }
+            }
+        };
+
+        // 遍历所有按钮数组并设置监听器
+        for (Button button : buttons) {
+            if (button != null) button.setOnClickListener(buttonClickListener);
+        }
+        for (Button button : function) {
+            if (button != null) button.setOnClickListener(buttonClickListener);
+        }
+    }
+
+    private void appendTextToCurrentSelectedTextView(String text) {
+        String currentText = num.getText().toString();
+        expression = currentText + text;
+        num.setText(expression);
+    }
+
+    private void handleButtonClick(Button button) {
+        String buttonText = button.getText().toString();
+        switch (button.getId()) {
+            case R.id.buttonClearAll:
+                clearAllText();
+                break;
+            case R.id.buttonClear:
+                clearLast();
+                break;
+            case R.id.buttonEqual:
+                break;
+            default:
+                appendTextToCurrentSelectedTextView(buttonText);
+                break;
+        }
+    }
+
+    private void clearAllText() {
+        if (num != null) {
+            expression = "";
+            num.setText("");
+        }
+    }
+
+    private void clearLast() {
+        if (num != null && num.length() > 0) {
+            String currentText = num.getText().toString();
+            expression = currentText.substring(0, currentText.length() - 1);
+            num.setText(expression);
+        }
     }
 
 }
--- app/src/main/java/com/example/superjjj/SystemActivity.java
+++ app/src/main/java/com/example/superjjj/SystemActivity.java
@@ -2,17 +2,57 @@ package com.example.superjjj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.transition.Slide;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.superjjj.util.ToastUtil;
+
+import java.math.BigInteger;
 
 public class SystemActivity extends AppCompatActivity {
     private View header;
     private View converButtons;
     private View back;
     private TextView text;
+    private View BIN;
+    private View OCT;
+    private View DEC;
+    private View HEX;
+
+    private TextView bin;
+    private TextView oct;
+    private TextView dec;
+    private TextView hex;
+
+    private TextView currentSelectedTextView = null;
+    private View currentSelectedLayout = null;
+
+    private String expression;
+
+
+
+    private Button[] function = new Button[3]; // 清0，回退，等号
+    private Button[] binary = new Button[3]; // 1，0，00 二进制
+    private Button[] octonary = new Button[6]; // 八进制 234567
+    private Button[] decimal = new Button[2]; // 十进制 89
+    private Button[] hexadecimal = new Button[6]; // 16进制ABCDEF
+
+
+
+    Button button0, button00, button1;
+    Button button2, button3, button4, button5, button6, button7;
+    Button button8, button9;
+    Button buttonA, buttonB, buttonC, buttonD, buttonE, buttonF;
+    Button clearAll, clear, equal;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +68,15 @@ public class SystemActivity extends AppCompatActivity {
 
         header = findViewById(R.id.header);
         converButtons = findViewById(R.id.converbutton2);
+        BIN = findViewById(R.id.BINView);
+        OCT = findViewById(R.id.OCTView);
+        DEC = findViewById(R.id.DECView);
+        HEX = findViewById(R.id.HEXView);
+
+        bin = findViewById(R.id.BINText);
+        oct = findViewById(R.id.OCTText);
+        dec = findViewById(R.id.DECText);
+        hex = findViewById(R.id.HEXText);
 
         text = findViewById(R.id.textView);
         text.setText("System Scale");
@@ -37,11 +86,282 @@ public class SystemActivity extends AppCompatActivity {
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 finish();
                 overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
             }
         });
+
+        button0 = findViewById(R.id.button0s);
+        button1 = findViewById(R.id.button1s);
+        button2 = findViewById(R.id.button2s);
+        button3 = findViewById(R.id.button3s);
+        button4 = findViewById(R.id.button4s);
+        button5 = findViewById(R.id.button5s);
+        button6 = findViewById(R.id.button6s);
+        button7 = findViewById(R.id.button7s);
+        button8 = findViewById(R.id.button8s);
+        button9 = findViewById(R.id.button9s);
+        button00 = findViewById(R.id.button00s);
+        buttonA = findViewById(R.id.buttonA);
+        buttonB = findViewById(R.id.buttonB);
+        buttonC = findViewById(R.id.buttonC);
+        buttonD = findViewById(R.id.buttonD);
+        buttonE = findViewById(R.id.buttonE);
+        buttonF = findViewById(R.id.buttonF);
+        clearAll = findViewById(R.id.buttonClearAlls);
+        clear = findViewById(R.id.buttonClears);
+        equal = findViewById(R.id.buttonEquals);
+
+        function[0] = clearAll;
+        function[1] = clear;
+        function[2] = equal;
+
+        binary[0] = button1;
+        binary[1] = button0;
+        binary[2] = button00;
+
+        octonary[0] = button2;
+        octonary[1] = button3;
+        octonary[2] = button4;
+        octonary[3] = button5;
+        octonary[4] = button6;
+        octonary[5] = button7;
+
+        decimal[0] = button8;
+        decimal[1] = button9;
+
+        hexadecimal[0] = buttonA;
+        hexadecimal[1] = buttonB;
+        hexadecimal[2] = buttonC;
+        hexadecimal[3] = buttonD;
+        hexadecimal[4] = buttonE;
+        hexadecimal[5] = buttonF;
+
+        initClickListeners(BIN, bin);
+        initClickListeners(OCT, oct);
+        initClickListeners(DEC, dec);
+        initClickListeners(HEX, hex);
+
+        changeTextSelected(bin);
+
+        setupButtonListeners();
     }
 
+    /**
+     * 得到对应的选中的layout和textview
+     * @param layout
+     * @param textView
+     */
+
+    private void initClickListeners(View layout, final TextView textView) {
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeTextSelected(textView);
+            }
+        };
+
+        layout.setOnClickListener(listener);
+        textView.setOnClickListener(listener);
+    }
+
+    /**
+     * 不断更换选中的textView
+     * @param textView
+     */
+    private void changeTextSelected(TextView textView) {
+        // 如果有先前选中的 TextView，将其颜色改回黑
+        if (currentSelectedTextView != null && currentSelectedTextView != textView) {
+            currentSelectedTextView.setTextColor(Color.BLACK);
+        }
+
+        // 设置新 TextView 的颜色并更新当前选中的 TextView
+        textView.setTextColor(Color.parseColor("#3B67E8"));
+        currentSelectedTextView = textView;
+
+        expression = currentSelectedTextView.getText().toString();
+
+        if (currentSelectedLayout != null) {
+            currentSelectedLayout.setBackgroundResource(0); // 移除边框
+        }
+
+        // 设置新选中的 LinearLayout 的背景
+        if (textView == bin) {
+            BIN.setBackgroundResource(R.drawable.border);
+            currentSelectedLayout = BIN;
+        } else if (textView == oct) {
+            OCT.setBackgroundResource(R.drawable.border);
+            currentSelectedLayout = OCT;
+        } else if (textView == dec) {
+            DEC.setBackgroundResource(R.drawable.border);
+            currentSelectedLayout = DEC;
+        } else if (textView == hex) {
+            HEX.setBackgroundResource(R.drawable.border);
+            currentSelectedLayout = HEX;
+        }
+
+
+        if (textView == bin) {
+            // BIN 被选中
+            /*
+            允许functions和0,1,00
+             */
+            setButtonsEnabled(binary, true);
+            setButtonsEnabled(octonary, false);
+            setButtonsEnabled(decimal, false);
+            setButtonsEnabled(hexadecimal, false);
+        } else if (textView == oct) {
+            // OCT 被选中
+            /*
+            允许functions和0,1,00,234567
+             */
+            setButtonsEnabled(binary, true);
+            setButtonsEnabled(octonary, true);
+            setButtonsEnabled(decimal, false);
+            setButtonsEnabled(hexadecimal, false);
+        } else if (textView == dec) {
+            // DEC 被选中
+            /*
+            允许functions和0,1,00,234567,89
+             */
+            setButtonsEnabled(binary, true);
+            setButtonsEnabled(octonary, true);
+            setButtonsEnabled(decimal, true);
+            setButtonsEnabled(hexadecimal, false);
+        } else if (textView == hex) {
+            // HEX 被选中
+            /*
+            允许所有
+             */
+            setButtonsEnabled(binary, true);
+            setButtonsEnabled(octonary, true);
+            setButtonsEnabled(decimal, true);
+            setButtonsEnabled(hexadecimal, true);
+        }
+
+        setButtonsEnabled(function, true);
+    }
+
+    private void setButtonsEnabled(Button[] buttons, boolean enabled) {
+        for (Button button : buttons) {
+            if (button != null) {
+                button.setEnabled(enabled);
+            }
+        }
+    }
+
+    private void setupButtonListeners() {
+        View.OnClickListener buttonClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currentSelectedTextView != null) {
+                    Button clickedButton = (Button) v;
+                    handleButtonClick(clickedButton);
+                }
+            }
+        };
+
+        // 遍历所有按钮数组并设置监听器
+        for (Button button : binary) {
+            if (button != null) button.setOnClickListener(buttonClickListener);
+        }
+        for (Button button : octonary) {
+            if (button != null) button.setOnClickListener(buttonClickListener);
+        }
+        for (Button button : decimal) {
+            if (button != null) button.setOnClickListener(buttonClickListener);
+        }
+        for (Button button : hexadecimal) {
+            if (button != null) button.setOnClickListener(buttonClickListener);
+        }
+        for (Button button : function) {
+            if (button != null) button.setOnClickListener(buttonClickListener);
+        }
+    }
+
+    private void appendTextToCurrentSelectedTextView(String text) {
+        String currentText = currentSelectedTextView.getText().toString();
+        expression = currentText + text;
+        currentSelectedTextView.setText(expression);
+    }
+
+    private void handleButtonClick(Button button) {
+        String buttonText = button.getText().toString();
+        switch (button.getId()) {
+            case R.id.buttonClearAlls:
+                clearAllText();
+                break;
+            case R.id.buttonClears:
+                clearLast();
+                break;
+            case R.id.buttonEquals:
+                getResult();
+                break;
+            default:
+                appendTextToCurrentSelectedTextView(buttonText);
+                break;
+        }
+    }
+
+    private void getResult() {
+        if (currentSelectedTextView != null && !expression.isEmpty()){
+            try {
+                long decimalValue;
+                if (currentSelectedTextView == bin){
+                    decimalValue = parseLongWithOverflowCheck(expression,2);
+                    oct.setText(Long.toOctalString(decimalValue));
+                    dec.setText(Long.toString(decimalValue));
+                    hex.setText(Long.toHexString(decimalValue).toUpperCase());
+                } else if (currentSelectedTextView == oct){
+                    decimalValue = parseLongWithOverflowCheck(expression,8);
+                    bin.setText(Long.toBinaryString(decimalValue));
+                    dec.setText(Long.toString(decimalValue));
+                    hex.setText(Long.toHexString(decimalValue).toUpperCase());
+                } else if (currentSelectedTextView == dec){
+                    decimalValue = parseLongWithOverflowCheck(expression,10);
+                    bin.setText(Long.toBinaryString(decimalValue));
+                    oct.setText(Long.toOctalString(decimalValue));
+                    hex.setText(Long.toHexString(decimalValue).toUpperCase());
+                } else if (currentSelectedTextView == hex){
+                    decimalValue = parseLongWithOverflowCheck(expression,16);
+                    bin.setText(Long.toBinaryString(decimalValue));
+                    oct.setText(Long.toOctalString(decimalValue));
+                    dec.setText(Long.toString(decimalValue).toUpperCase());
+                }
+            } catch (NumberFormatException e) {
+                ToastUtil.showShort(this, "Number too large or invalid format" );
+            }
+        }
+    }
+
+    /**
+     * the value might be too large, when i calculate the lager number the app might be crashed, so i
+     * use this to switch the expression
+     * @param value
+     * @param radix
+     * @return
+     * @throws NumberFormatException
+     */
+    private long parseLongWithOverflowCheck(String value, int radix) throws NumberFormatException {
+        BigInteger bigInt = new BigInteger(value, radix);
+        if (bigInt.compareTo(BigInteger.valueOf(Long.MAX_VALUE)) > 0 || bigInt.compareTo(BigInteger.valueOf(Long.MIN_VALUE)) < 0) {
+            throw new NumberFormatException("Number too large");
+        }
+        return bigInt.longValue();
+    }
+
+    private void clearAllText() {
+        if (currentSelectedTextView != null) {
+            expression = "";
+            currentSelectedTextView.setText("");
+        }
+    }
+
+    private void clearLast() {
+        if (currentSelectedTextView != null && currentSelectedTextView.length() > 0) {
+            String currentText = currentSelectedTextView.getText().toString();
+            expression = currentText.substring(0, currentText.length() - 1);
+            currentSelectedTextView.setText(expression);
+        }
+    }
 }
