61c67a1c85e24c72cb103776036fd22ed743dcc3
XuZhong <1574030430@qq.com>
2023-11-19 18:45:41 +0800

    2048 new layout

--- app/src/main/java/com/example/superjjj/Game2048Activity.java
+++ app/src/main/java/com/example/superjjj/Game2048Activity.java
@@ -1,12 +1,17 @@
 package com.example.superjjj;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
 import android.transition.Slide;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.TextView;
 
 public class Game2048Activity extends AppCompatActivity {
@@ -20,6 +25,9 @@ public class Game2048Activity extends AppCompatActivity {
 
     private Button restart;
     private Button undo;
+
+    private GridLayout gridLayout;
+    private final int GRID_SIZE = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,7 +62,49 @@ public class Game2048Activity extends AppCompatActivity {
         restart = findViewById(R.id.restart);
         undo = findViewById(R.id.undo);
 
+        gridLayout = findViewById(R.id.gameGrid);
+        gridLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                createGrid(gridLayout.getWidth());
+            }
+        });
 
+    }
 
+    private void createGrid(int width) {
+
+        // 格子间的margin
+        int tileMargin = getResources().getDimensionPixelSize(R.dimen.tile_margin); // 需要在dimens.xml定义
+        int paddingTotal = 2 * getResources().getDimensionPixelSize(R.dimen.padding); // 获取总padding（左右两侧）
+        int tileSize = (width - paddingTotal - tileMargin *  3) / GRID_SIZE;
+
+        for (int row = 0; row < GRID_SIZE; row++) {
+            for (int col = 0; col < GRID_SIZE; col++) {
+                TextView tile = new TextView(this);
+                tile.setWidth(tileSize);
+                tile.setHeight(tileSize);
+                tile.setGravity(Gravity.CENTER);
+                tile.setTextSize(TypedValue.COMPLEX_UNIT_SP, 24);
+                tile.setBackground(ContextCompat.getDrawable(this, R.drawable.gamebtn));
+
+                // 可以为每个tile设置标签或值
+                // 00 01 02 03
+                // 10 11 12 13
+                // 20 21 22 23
+                // 30 31 32 33
+                tile.setTag(R.id.tag_row, row);
+                tile.setTag(R.id.tag_col, col);
+
+                // 添加到GridLayout
+                gridLayout.addView(tile);
+
+                // 设置边距
+                GridLayout.LayoutParams params = (GridLayout.LayoutParams) tile.getLayoutParams();
+                int halfMargin = tileMargin / 2;
+                params.setMargins(halfMargin, halfMargin, halfMargin, halfMargin );
+                tile.setLayoutParams(params);
+            }
+        }
     }
 }
